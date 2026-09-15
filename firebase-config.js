fcaff00302ff1b086256c829f3b8380d87280780
asd00asdzz <mokll0081@gmail.com>
2026-09-15 12:48:11 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA2v01G-19ef65tZd8dRZndubDsnirqUoc",
+  authDomain: "get-startedfirebase-project.firebaseapp.com",
+  projectId: "get-startedfirebase-project",
+  storageBucket: "get-startedfirebase-project.firebasestorage.app",
+  messagingSenderId: "834488640705",
+  appId: "1:834488640705:web:81998ad4745996fbbf2040",
+  measurementId: "G-QFR3BG6HGK"
+};
+
+export const ADMIN_EMAIL = "mokll0081@gmail.com";
